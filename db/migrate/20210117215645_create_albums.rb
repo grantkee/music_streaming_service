5e9011dc63cc6d89a63325d3f48ed858c4ce3ab4
grantkee <grant.d.kee@gmail.com>
2021-01-17 16:03:53 -0600

    added string columns to migrate files

--- db/migrate/20210117215645_create_albums.rb
+++ db/migrate/20210117215645_create_albums.rb
@@ -1,7 +1,9 @@
 class CreateAlbums < ActiveRecord::Migration[6.0]
   def change
     create_table :albums do |t|
-
+      t.string :cover
+      t.string :title
+      t.string :artist
       t.timestamps
     end
   end
