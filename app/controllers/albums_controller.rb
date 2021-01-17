6c7c789f944de36d00ec281b27bd3d6ac78ea39a
grantkee <grant.d.kee@gmail.com>
2021-01-17 17:09:23 -0600

    updates to show

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,5 +1,12 @@
 class AlbumsController < ApplicationController
+
   def index
     @albums = Album.all
   end
+
+  def show
+    @album = Album.find(params[:id])
+    @tracks = @album.tracks
+  end
+
 end
